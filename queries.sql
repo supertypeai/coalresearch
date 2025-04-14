7e2335430fe891283d8dd432087a20b818865482
unknown <ronkcloud@gmail.com>
2025-04-14 10:29:48 +0700

    update

---
+++ queries.sql
@@ -0,0 +1,51 @@
+SELECT
+	c.id as company_id,
+    c.name AS company_name,
+    c.company_type,
+    co.parent_company_id, 
+    parent.name AS parent_company_name,
+    COALESCE(parent3.name, parent2.name, parent.name) AS company_group
+FROM
+    company c
+LEFT JOIN 
+    company_ownership co ON co.child_company_id = c.id
+LEFT JOIN
+	company parent ON parent.id = co.parent_company_id 
+LEFT JOIN
+	company_ownership co2 ON co2.child_company_id = co.parent_company_id
+LEFT JOIN
+	company parent2 ON parent2.id = co2.parent_company_id
+LEFT JOIN
+	company_ownership co3 ON co3.child_company_id = co2.parent_company_id
+LEFT JOIN
+	company parent3 ON parent3.id = co3.parent_company_id
+	
+SELECT 
+	c.id AS company_id,
+    c.name AS company_name,
+    c.operation_location,
+    c.key_operation,
+    c.company_type,
+    p.year,
+    p.production_volume,
+    p.strip_ratio,
+    p.overburden_removal_volume,
+    p.sales_volume,
+    p.mineral_type,
+    p.calorific_value,
+    p.mining_operation_status,
+    p.reserve,
+    p.resource
+FROM
+    company c
+LEFT JOIN
+    coal_company_performance p ON c.id = p.company_id
+	
+--INSERT INTO company (name, name_short, idx_ticker, operation_location, representative_address, company_type, key_operation)
+--VALUES
+--    ('Alam Tri Abadi', 'ATA', NULL, NULL, NULL, 'Trader', 'Trading')
+--    
+--    
+--INSERT INTO company_ownership (parent_company_id, child_company_id, percentage_ownership)
+--VALUES
+--    (1, 3, NULL)
